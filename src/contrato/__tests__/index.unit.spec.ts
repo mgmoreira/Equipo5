12f2a7343d85efac1b99edea69d78065457304b1
LUIS <luis.lozano@uc.cl>
2022-06-03 15:06:57 -0400

    test

--- src/contrato/__tests__/index.unit.spec.ts
+++ src/contrato/__tests__/index.unit.spec.ts
@@ -1,19 +1,41 @@
-import * as contract from "../assembly";
+import {
+  setSala,
+  getSalas,
+} from "../assembly";
+import { VMContext, PersistentVector } from "near-sdk-as";
+import { Alumnos, Sala } from "../assembly/model";
 
-describe("Contract", () => {
-  // VIEW method tests
+const FECHA = "2022-06-02";
 
-  it("says hello", () => {
-    expect(contract.helloWorld()).toStrictEqual("hello world")
-  })
+const setContext = (): void => {
+  //Variables del contexto
+  VMContext.setSigner_account_id("profesor");
+};
 
-  it("reads data", () => {
-    expect(contract.read("some key")).toStrictEqual("🚫 Key [ some key ] not found in storage. ( storage [ 0 bytes ] )")
-  })
 
-  // CHANGE method tests
+describe("SetSalaInicial", () => {
+  it("Registra una sala con un profesor asociado sin alumnos porque estÃ¡ dando de alta la sala.", () => {
 
-  it("saves data to contract storage", () => {
-    expect(contract.write("some-key", "some value")).toStrictEqual("✅ Data saved. ( storage [ 18 bytes ] )")
-  })
+    setContext();
+
+    setSala(FECHA);
+    // let v = new PersistentVector<Sala>("s")
+    var salitas: Sala[] = getSalas();
+    var alumnos = new Array<Alumnos>();
+    for (let index = 0; index < salitas.length; index++) {
+      const element = salitas[index];
+      // console.log(element.profesor)
+      expect(element.profesor).toBe("profesor");
+      expect(element.fecha).toBe(FECHA);
+      expect(element.alumnos.length).toBe(0);
+      expect(element.covid).toBe(false);
+    }
+    // if (salitas) {
+    //   expect(salita[0].profesor).toBe("profesor");
+    //   expect(salita[0].fecha).toBe(FECHA);
+    //   expect(salita[0].alumnos).toBe([]);
+    //   expect(salita[0].covid).toBe(false);
+    // }
+
+  });
 })
